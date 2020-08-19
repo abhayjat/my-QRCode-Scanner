26006ea7b005c97ffb21cc772c177e4aa833174b
abhayjat <abhay.jat@outlook.com>
2020-08-19 21:59:06 +0530

    Make QR Genrater

--- app/src/main/java/com/example/myqrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/myqrcodescanner/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.myqrcodescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText qrValue;
+    Button  btngenrator,btnscan;
+    ImageView qrimgGen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrValue=(EditText) findViewById(R.id.enter_val);
+        btngenrator  =(Button) findViewById(R.id.genratebtn);
+        qrimgGen=(ImageView) findViewById(R.id.scaGan);
+        btnscan = (Button) findViewById(R.id.scanbtn);
+
+        btngenrator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data=qrValue.getText().toString();
+                if(data.isEmpty()){
+                    qrValue.setError("Enter some value");
+                }else {
+                    QRGEncoder qrgEncoder=new QRGEncoder(data,null, QRGContents.Type.TEXT,500);
+                    Bitmap bitmap;
+                    try {
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrimgGen.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
