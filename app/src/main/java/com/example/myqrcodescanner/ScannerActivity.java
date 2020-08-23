613092063f410a35418684b10e8ec751e0c19509
abhayjat <abhay.jat@outlook.com>
2020-08-24 00:08:03 +0530

    Add Gallery Image QR code Scanner make Workable

--- app/src/main/java/com/example/myqrcodescanner/ScannerActivity.java
+++ app/src/main/java/com/example/myqrcodescanner/ScannerActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,13 +19,23 @@ import android.widget.Toast;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+//import com.muddzdev.styleabletoast.StyleableToast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Reader;
 
 
 public class ScannerActivity extends AppCompatActivity {
@@ -41,6 +54,8 @@ public class ScannerActivity extends AppCompatActivity {
         resultScan=(TextView) findViewById(R.id.resultsc);
         codeScanner=new CodeScanner(this,scannerView);
         gallery=(ImageButton) findViewById(R.id.gallery);
+
+
         codeScanner.setDecodeCallback(new DecodeCallback() {        //Use setDecodeCallback for convert our scan code.
             @Override
             public void onDecoded(@NonNull final Result result) {
@@ -104,4 +119,67 @@ public class ScannerActivity extends AppCompatActivity {
 
         startActivityForResult(pickIntent, 111);
     }
+
+    @Override
+
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            try {
+
+                final Uri imageUri = data.getData();
+
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                try {
+
+                    Bitmap bMap = selectedImage;
+
+                    String contents = null;
+
+
+
+                    int[] intArray = new int[bMap.getWidth()*bMap.getHeight()];
+
+                    bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+
+                    LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+
+                    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+                    MultiFormatReader reader = new MultiFormatReader();
+
+                    Result result = reader.decode(bitmap);
+
+                    contents = result.getText();
+
+                    Toast.makeText(getApplicationContext(),contents,Toast.LENGTH_LONG).show();
+                    
+                }catch (Exception e){
+
+                    e.printStackTrace();
+
+                }
+                //  image_view.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+                Toast toast = null;
+                Toast.makeText(ScannerActivity.this, "Something went wrong", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        }else {
+
+            Toast.makeText(ScannerActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
 }
